a9eacb645d6292a52e656206c15f1d535469f22a
MichaelZhegalin <mixail.zhegalin@mail.ru>
2021-09-12 22:09:49 +0300

    Исправил недочеты

--- js/js.js
+++ js/js.js
@@ -4,21 +4,12 @@ let form = document.querySelector('form');
 let text_warning = document.querySelector(".warning");
 
 text_warning.hidden = true;
-button.disabled = true
-
-let my_func = function(event) {
-    event.preventDefault();
-};
 
 function check_len(){
-	res = 0;
-	console.log(password.value.length);
-	if (password.value.length >= 6) res = 1;
-
-	return res;
+	if (password.value.length >= 6) return 1;
 }
 
-button.onmouseover = (e) =>{
+password.onchange = (e) =>{
 	if (check_len()){
 	 	text_warning.hidden = true;
 	 	button.disabled = false;
@@ -30,5 +21,5 @@ button.onmouseover = (e) =>{
 }
 
 button.onclick = (e) =>{
-	form.onsubmit = event.preventDefault();
+	event.preventDefault();
 }
